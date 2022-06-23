b57b1b9cec24682759a6b6fbc4680230946eeec8
Borges <premiersoft-paulo.borges@ailos.coop.br>
2022-06-23 15:49:28 -0300

    refactor: remove unused class

--- app/src/main/java/com/pvsb/ecommercewhitelabel/presentation/viewmodel/HomeVIewModel.kt
+++ app/src/main/java/com/pvsb/ecommercewhitelabel/presentation/viewmodel/HomeVIewModel.kt
@@ -6,14 +6,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.pvsb.ecommercewhitelabel.data.model.ProductDTO
 import com.pvsb.ecommercewhitelabel.domain.HomeRepository
-import com.pvsb.ecommercewhitelabel.domain.HomeUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeVIewModel @Inject constructor(
-    private val homeUseCase: HomeUseCase,
     private val repository: HomeRepository
 ) : ViewModel() {
 
